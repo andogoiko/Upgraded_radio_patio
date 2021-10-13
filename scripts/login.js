d8cd6390a7ac07dcf56457982eabe1622b3d57d5
mmozos <martinmozos@gmail.com>
2021-10-13 10:02:52 +0200

    Second login

--- scripts/login.js
+++ scripts/login.js
@@ -3,59 +3,53 @@
 
 let current = null;
 
-//if (document.querySelector('#email')) {
-  document.querySelector('#email').addEventListener('focus', e => {
-    if (current) current.pause();
-    current = anime({
-      targets: 'path',
-      strokeDashoffset: {
-        value: 0,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '240 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
-    });
+document.querySelector('#email').addEventListener('focus', e => {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
   });
-//}
+});
 
-//if (document.querySelector('#password')) {
-  document.querySelector('#password').addEventListener('focus', e => {
-    if (current) current.pause();
-    current = anime({
-      targets: 'path',
-      strokeDashoffset: {
-        value: -336,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '240 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
-    });
+document.querySelector('#password').addEventListener('focus', e => {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
   });
-//}
+});
 
-//if (document.querySelector('#submit')) {
-  document.querySelector('#submit').addEventListener('focus', e => {
-    if (current) current.pause();
-    current = anime({
-      targets: 'path',
-      strokeDashoffset: {
-        value: -730,
-        duration: 700,
-        easing: 'easeOutQuart'
-      },
-      strokeDasharray: {
-        value: '530 1386',
-        duration: 700,
-        easing: 'easeOutQuart'
-      }
-    });
+document.querySelector('#submit').addEventListener('focus', e => {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -730,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '530 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
   });
-//}
+});
